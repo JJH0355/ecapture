34b316d2f90046ec01b7dbe09094c340b4b5ad54
JJH0355 <180150445+JJH0355@users.noreply.github.com>
2025-08-20 14:08:36 +0900

    루프 추가

--- tests/python3_https.py
+++ tests/python3_https.py
@@ -1,13 +1,16 @@
-# python3 https_client.py
+# python3 https_client_long.py
 import urllib.request
 import urllib.error
+import time
 
-try:
-    response = urllib.request.urlopen('https://www.baidu.com')
-    print('response headers: "%s"' % response.info())
-    print('response headers: "%s"' % response.read().decode())
+while True:
+    try:
+        response = urllib.request.urlopen('https://www.baidu.com')
+        print('response headers: "%s"' % response.info())
+        print('response body (100 chars): "%s"' % response.read().decode()[:100])
+    except urllib.error.HTTPError as e:
+        print('http error code: ', e.code)
+    except urllib.error.URLError as e:
+        print("can't connect, reason: ", e.reason)
 
-except urllib.error.HTTPError as e:
-    print('http error code: ', e.code)
-except urllib.error.URLError as e:
-    print("can't connect, reason: ", e.reason)
+    time.sleep(5)  # 5초마다 반복 요청
